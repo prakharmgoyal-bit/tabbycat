ad7d5f91e8769f818317599790cf9c9befb2bef5
Prakhar Goyal <prakhar.mgoyal@gmail.com>
2026-04-04 14:05:03 +0530

    fix database and redis urls for Railway

--- tabbycat/settings/docker.py
+++ tabbycat/settings/docker.py
@@ -3,39 +3,46 @@
 # ==============================================================================
 
 import os
+import dj_database_url
 
 ALLOWED_HOSTS = ["*"]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tabbycat',
-        'USER': 'tabbycat',
-        'PASSWORD': 'tabbycat',
-        'HOST': 'db',
-        'PORT': 5432, # Non-standard to prevent collisions,
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'tabbycat',
+            'USER': 'tabbycat',
+            'PASSWORD': 'tabbycat',
+            'HOST': 'db',
+            'PORT': 5432,
+        }
+    }
+
+redis_url = os.environ.get('REDIS_URL', 'redis://redis:6379')
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": redis_url + "/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "SOCKET_CONNECT_TIMEOUT": 5,
+            "SOCKET_TIMEOUT": 60,
+        },
+    },
 }
 
-if bool(int(os.environ['DOCKER_REDIS'])) if 'DOCKER_REDIS' in os.environ else False:
-    CACHES = {
-        "default": {
-            "BACKEND": "django_redis.cache.RedisCache",
-            "LOCATION": "redis://redis:6379/1",
-            "OPTIONS": {
-                "CLIENT_CLASS": "django_redis.client.DefaultClient",
-                "SOCKET_CONNECT_TIMEOUT": 5,
-                "SOCKET_TIMEOUT": 60,
-            },
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [redis_url],
+            "group_expiry": 10800,
         },
-    }
-
-    CHANNEL_LAYERS = {
-        "default": {
-            "BACKEND": "channels_redis.core.RedisChannelLayer",
-            "CONFIG": {
-                "hosts": [("redis", 6379)],
-                "group_expiry": 10800,
-            },
-        },
-    }
+    },
+}
